38fee18b4c3323752137e2bfe4cb7191f7b8faa9
孔冬滨 <dbkong@leqee.com>
2017-04-19 18:17:42 +0800

    修改积分查询列表 以时间做第一级

--- src/view/earnRecord.vue
+++ src/view/earnRecord.vue
@@ -1,54 +1,148 @@
 <template>
 	<div id="earnRecord">
-		<div class="navbar">
-			<div class="nav-item" :class="{active: type=='earn'}" @click.prevent='changeItem("earn")'>获取</div>
-			<div class="nav-item" :class="{active: type=='use'}" @click.prevent='changeItem("use")'>使用</div>
+		<div class="swiper-container">
+		    <div class="swiper-wrapper">
+		        <div class="swiper-slide">
+		        	<div class="mainBox">
+						<div class="timeline" @click.prevent.stop='changeTime'>
+							{{time}}
+							<input type="hidden" id='picker' v-model='time'>
+						</div>
+						<div class="line"></div>
+						<div class="pointsBox">
+							<div class='title left'>本月积分</div>
+				        	<div class='totalPoints'>
+				        		<div class="flex">
+					        		<span class='text-note'>获取：</span> <span class='text-pink'>{{earnPoints}}</span>        			
+				        		</div>
+				        		<div class="flex">
+					        		<span class='text-note'>使用：</span> {{usePoints}}        			
+				        		</div>
+				        	</div>
+						</div>
+						<div class="line"></div>
+						<div class="navbar">
+							<div class="nav-item" :class="{active: type=='earn'}" @click.prevent='changeItem("earn")'>获取</div>
+							<div class="nav-item" :class="{active: type=='use'}" @click.prevent='changeItem("use")'>使用</div>
+						</div>
+						<div class="line"></div>
+						<div class="list">
+							<ul>
+								<li v-for='item in list'>
+									<div class="way">
+										<p v-text='item.name'></p>
+										<p class='time' v-text='item.time'></p>
+									</div>
+									<div class="num"><template v-if='type=="earn"'>+</template><template v-else>-</template>{{item.num}}</div>
+								</li>
+							</ul>
+						</div>			        		
+		        	</div>
+				</div>
+		    </div>
 		</div>
-		<div class="weui-cells timeline">
-			<div class="weui-cell weui-cell_access">
-				<div class="weui-cell__hd">
-	                <label class="weui-label" style='width: 50px;'>本月</label>
-	            </div>
-	            <div class="weui-cell__bd">
-	            	<p v-if="type=='earn'"><span class='text-note'>获取：</span> <span class='text-pink'>{{totalNum}}</span></p>
-	            	<p v-else><span class='text-note'>使用：</span> {{totalNum}}</p>
-	            </div>
-	            <div class="weui-cell__ft">2017年4月</div>
-			</div>
-		</div>
-		<div class="list">
-			<ul>
-				<li v-for='item in list'>
-					<div class="way">
-						<p v-text='item.name'></p>
-						<p class='time' v-text='item.time'></p>
-					</div>
-					<div class="num"><template v-if='type=="earn"'>+</template><template v-else>-</template>{{item.num}}</div>
-				</li>
-			</ul>
-		</div>
+		
 	</div>
 </template>
 
 <script>
 	import {setNum} from '../common/common'
 	import {mapState, mapMutations} from 'vuex'
+	import 'src/plugins/swiper.min.js'
+	import 'src/style/swiper.min.css'
 	export default{
 		mounted() {
+			let _self = this;
 			this.SET_TITLE('积分记录');
 			this.getList();
+			$("#picker").picker({
+				title: "选择年月",
+				cols: [
+					{
+						textAlign: 'center',
+						displayValues: ['2016年', '2017年', '2018年'],
+						values: [2016,2017,2018]
+					},
+					{
+						textAlign: 'center',
+						displayValues: ['1月','2月','3月','4月','5月','6月','7月','8月','9月','10月','11月','12月'],
+						values: [1,2,3,4,5,6,7,8,9,10,11,12]
+					}
+				],
+				onChange(a,b,c){
+					_self.year = b[0];
+					_self.month = b[1];
+					_self.year_show = c[0];
+					_self.month_show = c[1];
+				},
+				onClose(){
+					_self.hasChange = true;
+				}
+			});
+			//初始化swiper
+        	new Swiper('.swiper-container', {
+        		onSetTranslate (swiper,translate){
+        			// console.log('偏移量被设置为'+translate);
+        			if (_self.slideBoolean&&Math.abs(translate)>80) {
+        				_self.slideBoolean = false;
+        				$('.mainBox').fadeOut(100,()=>{
+	        				if (translate<0) {
+	        					if(_self.month==12){
+	        						_self.month = 1;
+	        						_self.year = Number(_self.year) + 1;
+	        					}else{
+	        						_self.month = Number(_self.month) + 1;
+	        					}
+	        				}else{
+	        					if(_self.month==1){
+	        						_self.month = 12;
+	        						_self.year = Number(_self.year) - 1;
+	        					}else{
+	        						_self.month = Number(_self.month) - 1;
+	        					}
+	        				}        					
+        				});
+        			}
+				},
+				onTransitionEnd (swiper) {
+					$('.mainBox').fadeIn(100);
+					_self.slideBoolean = true;
+				}
+		    });
 		},
 		data() {
 			return{
-				type: 'earn',
-
+				type: null,
+				month: null,
+				year: null,
 				list: [],
-				totalNum: null,
+				earnPoints: null,
+				usePoints: null,
+				hasChange: false,
+				slideBoolean: true
 			}
+		},
+		created(){
+			let date = new Date();
+			this.year = date.getFullYear();
+			this.month = date.getMonth() + 1;
+
+			this.type = 'earn';
+		},
+		computed:{
+			year_show(){
+				return this.year + '年'
+			},
+			month_show(){
+				return this.month + '月'
+			},
+			time (){
+				return this.year_show + this.month_show
+			},
 		},
 		methods: {
 			...mapMutations(['SET_TITLE']),
-			getList(){
+			async getList(){
 				let list = [{
 					name: '消费积分 (天猫旗舰店)',
 					time: '2017-04-14',
@@ -61,7 +155,8 @@
 				list.forEach((item)=>{
 					item.num = setNum(item.num);
 				})
-				this.totalNum = setNum(1005);
+				this.earnPoints = setNum(1005);
+				this.usePoints = setNum(3000);
 				this.list = list;
 			},
 			changeItem(_type){
@@ -77,7 +172,6 @@
 						time: '2017-03-06',
 						num: '5',
 					}];
-					this.totalNum = setNum(1005);
 				}else{
 					list = [{
 						name: '积分兑礼',
@@ -88,21 +182,34 @@
 						time: '2017-03-26',
 						num: '2000',
 					}];
-					this.totalNum = setNum(3000);
 				}
 				list.forEach((item)=>{
 					item.num = setNum(item.num);
 				})
 				this.list = list;
+			},
+			changeTime(){
+				var _self = this;
+				$('#picker').trigger('click');
+				$("#picker").picker("setValue", [_self.year,_self.month]);
 			}
 
+		},
+		watch:{
+			hasChange(newVal){
+				if (newVal) {
+					//await this.getList();
+					console.log('触发搜索');
+					this.hasChange = false;
+				}
+			}
 		}
 	}
 </script>
 
 <style lang="scss" scoped>
 	@import '../style/mixin';
-	
+
 	#earnRecord{
 		width: 100%;
 		min-height: 100%;
@@ -126,9 +233,26 @@
 		}
 	}
 	.timeline{
-		height: 1.706rem;
-		margin-top: 0;
-		font-size: 0.512rem;
+		font-size: 1rem;
+		text-align: center;
+	}
+	.pointsBox{
+		height: 1.28rem;
+		line-height: 1.28rem;
+		.title{
+			font-size: 0.64rem;
+			margin-left: 0.64rem;
+		}
+		.totalPoints{
+			display: flex;
+			.flex{
+				flex: 1;
+				text-align: center;
+			}
+		}
+	}
+	.line{
+		height: 0.2rem;
 		background-color: #f5f5f5;
 	}
 	
