79413426172acee88654e5a37b09603f6c7bf20c
孔冬滨 <dbkong@leqee.com>
2017-04-21 17:35:14 +0800

    add master_config

---
+++ config/master_config.js
@@ -0,0 +1,11 @@
+export default{
+	baseUrl: "http://testtianfuweishopapi.leqeewechat.com",
+	baseMobileUrl: "http://testtianfuweishop.leqeewechat.com/",
+	domainUrl:"leqeewechat.com",
+	oauthUrl: "https://open.weixin.qq.com/connect/oauth2/authorize?response_type=code&scope=snsapi_base&state=123&",
+	shopAppId: 3,
+	appId: "wx4978ebc6dae349c1",
+	shop_name: "祈福",
+	shop_id: "1",
+	is_debug: 0,
+}
